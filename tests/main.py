d6899514cdf1cf10ab21238f9265274d5056235b
21t2135dMakinoYuki <muyexiong@gmail.com>
2024-08-19 16:42:05 +0900

    CI test

---
+++ tests/main.py
@@ -0,0 +1,38 @@
+import unittest
+
+from speciallecture.CSVPrinter import CSVPrinter
+
+class TestCSVPrinter(unittest.TestCase):
+    def test_read(self):
+        printer = CSVPrinter("sample.csv")
+        line =printer.read()
+        print(line)
+        self.assertEqual(3, len(line))  # add assertion here
+
+    def test1(self):
+        printer = CSVPrinter("sample.csv")
+        line =printer.read()
+        print(line)
+        self.assertEqual(3, len(line))  # add assertion here
+
+    def test2(self):
+        printer = CSVPrinter("sample.csv")
+        lines = printer.read()
+        print(lines)
+
+        for line in lines:
+            self.assertEqual(4, len(line))
+
+
+    def test3(self):
+        try:
+            printer = CSVPrinter("sample1.csv")
+            printer.read()
+            unittest.TestCase.fail("This line should not be invoked")
+        except FileNotFoundError as e:
+            pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
